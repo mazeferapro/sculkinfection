d2fa64e27210c6295e1745f532b76046b3a3810e
Exavi <Assassins77718@gmail.com>
2025-08-19 19:21:06 +0300

    fix

--- src/main/java/mazefera/sculkInfection/SculkInfection.java
+++ src/main/java/mazefera/sculkInfection/SculkInfection.java
@@ -43,10 +43,6 @@ public class SculkInfection extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("EnhancedSculk Plugin вимкнено!");
-        if (visualEffects != null) {
-            visualEffects.cleanup(); // Критично важливо!
-            visualEffects.stopAmbientEffects();
-        }
     }
 
     public static SculkInfection getInstance() {
